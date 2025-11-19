52e8d92ea1d59569feb81e5beb96130b0a228668
Gustasilvadev <gustamarsil@gmail.com>
2025-11-19 10:58:50 -0300

    WebPage no Backend

---
+++ getmoneyBackend/src/main/java/com/getmoney/controller/WebController.java
@@ -0,0 +1,25 @@
+package com.getmoney.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@CrossOrigin("*")
+public class WebController {
+
+    @GetMapping("/")
+    public String home() {
+        return "forward:/index.html";
+    }
+
+    @GetMapping("/install")
+    public String install() {
+        return "forward:/install.html";
+    }
+
+    @GetMapping("/download-apk")
+    public String downloadApk() {
+        return "redirect:/app-release.apk";
+    }
+}
