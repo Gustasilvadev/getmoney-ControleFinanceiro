af3d3e53abe5d7fc5e93dd744a86e6235d375026
Gustasilvadev <gustamarsil@gmail.com>
2025-12-02 11:23:37 -0300

    Atualizado 2.1

--- getmoneyBackend/src/main/java/com/getmoney/controller/WebController.java
+++ getmoneyBackend/src/main/java/com/getmoney/controller/WebController.java
@@ -1,34 +1,21 @@
 package com.getmoney.controller;
 
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.net.MalformedURLException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-@RestController
+@Controller
+@CrossOrigin("*")
 public class WebController {
 
-    @GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
-    public ResponseEntity<String> home() throws Exception {
-        ClassPathResource resource = new ClassPathResource("static/index.html");
-        InputStream inputStream = resource.getInputStream();
-        String body = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
-        return ResponseEntity.ok(body);
+    @GetMapping("/")
+    public String home() {
+        return "forward:/index.html";
     }
 
-    @GetMapping(value = "/install", produces = MediaType.TEXT_HTML_VALUE)
-    public ResponseEntity<String> install() throws Exception {
-        ClassPathResource resource = new ClassPathResource("static/install.html");
-        InputStream inputStream = resource.getInputStream();
-        String body = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
-        return ResponseEntity.ok(body);
+    @GetMapping("/install")
+    public String install() {
+        return "forward:/install.html";
     }
+    
 }
